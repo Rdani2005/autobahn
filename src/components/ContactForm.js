b74894d232b22ab1df4f88d4fd7e499c35c82da5
rdani2005 <dricardosc15@gmail.com>
2022-02-10 07:44:19 -0600

    Changed the phone number

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,7 +8,7 @@ const ContactForm = () => {
                 <div className="card mx-4 my-4 bg-cristal-dark text-white col-6 col-md-6 p-4 w-75 curved">
                     <h1>Información de Contacto</h1>
                     <hr />
-                    <p><i className="bi bi-phone"></i> <a className="link-primary" href="tel:+50660408440">6040-8440</a></p>
+                    <p><i className="bi bi-phone"></i> <a className="link-primary" href="tel:+50622153559">+(506) 2215-3559</a></p>
                     <p><i className="bi bi-whatsapp"></i> <a className="link-primary" href="https://wa.me/50660408440?text=Hola!,%20Estoy%20interesado%20en%20hacer%20Negocios%20Contigo!!!">6040-8440</a></p>
                     <p><i class="bi bi-envelope-check"></i> <a className="link-primary" href="mailto:ricardo@grupoautobahn.com">ricardo@grupoautobahn.com</a></p>
                     <p>Dirección de la empresa</p>
